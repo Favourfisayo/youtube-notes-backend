3e71a6ba7a0e6384ee23dc0d21902ec5a3920a60
Favourfisayo <Favourfisayo5@gmail.com>
2025-04-19 00:12:00 +0100

    Updated Python code to use proxies

--- get_transcript.py
+++ get_transcript.py
@@ -1,11 +1,20 @@
 import sys
 import json
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.proxies import GenericProxyConfig
 
 video_id = sys.argv[1]
 
+ytt_api = YouTubeTranscriptApi(
+    proxy_config=GenericProxyConfig(
+        http_url="http://87.238.192.63:39272",
+        https_url="https://87.238.192.63:39272",
+    )
+)
+
+
 try:
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ytt_api.get_transcript(video_id)
     texts = " ".join([entry['text'] for entry in transcript])
     print(json.dumps(texts))
 except Exception as e:
