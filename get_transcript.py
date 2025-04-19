912eeb428006a7f77cb9bb44b9b2f3859ac02de7
Favourfisayo <Favourfisayo5@gmail.com>
2025-04-19 01:04:54 +0100

    Temporarily Reverting to Local development

--- get_transcript.py
+++ get_transcript.py
@@ -5,16 +5,17 @@ from youtube_transcript_api.proxies import GenericProxyConfig
 
 video_id = sys.argv[1]
 
-ytt_api = YouTubeTranscriptApi(
-    proxy_config=GenericProxyConfig(
-        http_url="http://87.238.192.63:39272",
-        https_url="https://87.238.192.63:39272",
-    )
-)
+# Future proxy configuration
+# ytt_api = YouTubeTranscriptApi(
+#     proxy_config=GenericProxyConfig(
+#         http_url="http://87.238.192.63:39272",
+#         https_url="https://87.238.192.63:39272",
+#     )
+# )
 
 
 try:
-    transcript = ytt_api.get_transcript(video_id)
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
     texts = " ".join([entry['text'] for entry in transcript])
     print(json.dumps(texts))
 except Exception as e:
